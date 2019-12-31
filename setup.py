9a6b515081fc7e6c450fc8d84b7e5c5e85a6d5f9
Fabio San <fsan@fbi>
2019-12-31 16:32:33 -0300

    Preparing prerelease 0.0.1-beta

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ setup(
   license='MIT',
   description = 'Small pipeline organizer for multiprocessing execution',
   author = 'fsan',
-  author_email = 'pabyo sansinaeti at g mail com',
+  author_email = "pabyo.sansinaeti@gmail.com",
   url = 'https://github.com/fsan/pyperaptor',
-  download_url = 'https://github.com/fsan/pyperaptor/releases/download/0.0.1-beta/pyperaptor-0.0.1.tar.gz'
+  download_url = 'https://github.com/fsan/pyperaptor/releases/download/0.0.1-beta/pyperaptor-0.0.1.tar.gz',
   keywords = ['pipeline', 'dinossaurs', 'threading'],
   install_requires=[
       ],
